4adf810234ebae8370bd2f6e73f29c40da5f6910
akashcool2014.av@gmail.com <akash.verma@careerninja.in>
2024-03-31 02:46:01 -0400

    added hover effects to tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,8 +9,8 @@ const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology, index) => (
-        <motion.div variants={fadeIn("down", "spring", index * 0.1, 0.75)} className='flex flex-col justify-between w-28 h-28' key={technology.name}>
-          <img src={technology.icon} />
+        <motion.div variants={fadeIn("down", "spring", index * 0.1, 0.75)} className='flex flex-col justify-between w-28 h-28 hover:text-[#915EFF]' key={technology.name}>
+          <img src={technology.icon} className="hover:scale-110"/>
           <p className="text-center">{technology.name}</p>
         </motion.div>
       ))}
