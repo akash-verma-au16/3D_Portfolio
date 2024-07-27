1a6a2a86ea9f2853ace877f2d162929fc5452d37
akashcool2014.av@gmail.com <akash.verma@careerninja.in>
2024-07-27 00:32:54 -0400

    added new animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,56 +1,62 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
-import { profile } from "../assets";
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
+import { profile } from '../assets';
 
 const Hero = () => {
-  return (
-    <section className={`relative w-full h-screen mx-auto`}>
-      <div className={`absolute inset-0 top-[80px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
-        <div className="hidden mt-5 md:flex flex-col items-center">
-          <div className="w-28 h-28 lg:w-48 lg:h-48 rounded-full overflow-hidden ">
-            <img className="w-full h-full object-cover" src={profile} alt="Profile" />
-          </div>
-        </div>
+	return (
+		<section className={`relative w-full h-screen mx-auto`}>
+			<div
+				className={`absolute inset-0 top-[80px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+				<div className="hidden mt-5 md:flex flex-col items-center">
+					<div className="w-28 h-28 lg:w-48 lg:h-48 rounded-full overflow-hidden ">
+						<img
+							className="w-full h-full object-cover"
+							src={profile}
+							alt="Profile"
+						/>
+					</div>
+				</div>
 
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
+				<div className="flex flex-col justify-center items-center mt-5">
+					<div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+				</div>
 
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Akash</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Full Stack Software Engineer <br className='sm:block hidden' />
-            and Cloud Developer
-          </p>
-        </div>
-      </div>
-      
-      <ComputersCanvas />
+				<div>
+					{/* animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-8 */}
+					<h1 className={`${styles.heroHeadText} text-white`}>
+						Hi, I'm <span className="text-[#915EFF]">Akash</span>
+					</h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
+						Full Stack Software Engineer <br className="sm:block hidden" />
+						and Cloud Developer
+					</p>
+				</div>
+			</div>
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
-    </section>
-  );
+			<ComputersCanvas />
+
+			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+				<a href="#about">
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+						<motion.div
+							animate={{
+								y: [0, 24, 0],
+							}}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: 'loop',
+							}}
+							className="w-3 h-3 rounded-full bg-secondary mb-1"
+						/>
+					</div>
+				</a>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
